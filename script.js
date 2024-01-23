926c9c73314eb54872da09245fc99454a674ed06
Vanvelson <ivanlobur5@gmail.com>
2024-01-23 13:58:28 +0200

    1234567

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("open_module").onclick = function(){
+    document.getElementById("module").classList.remove("hide")
+}
+document.getElementById("close_module").onclick = function(){
+    document.getElementById("module").classList.add("hide")
+}
